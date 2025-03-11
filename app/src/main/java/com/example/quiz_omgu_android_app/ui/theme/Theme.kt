541fef22ccc929662a36c4be63b21a730e074922
Egor Bugay <egbugaj@gmail.com>
2025-03-11 19:16:57 +0600

    chore: add Shapes to Theme file

--- app/src/main/java/com/example/quiz_omgu_android_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/quiz_omgu_android_app/ui/theme/Theme.kt
@@ -1,15 +1,23 @@
 package com.example.quiz_omgu_android_app.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(32.dp),
+)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
