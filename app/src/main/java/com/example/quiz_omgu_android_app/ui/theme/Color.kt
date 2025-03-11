5393b7c62c6415d182d133e504c0c9e75a381f91
Egor Bugay <egbugaj@gmail.com>
2025-03-11 18:04:09 +0600

    chore: configure color scheme and theme

--- app/src/main/java/com/example/quiz_omgu_android_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/quiz_omgu_android_app/ui/theme/Color.kt
@@ -2,6 +2,16 @@ package com.example.quiz_omgu_android_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Pink = Color(0xFFD698E6)
+val LightGreen = Color(0xFFD9EFDA)
+val Green = Color(0xFF8EBDAF)
+val TextGreen = Color(0xFF477466)
+val White = Color(0xFFFFFFFF)
+val LightBlue = Color(0xFFDBF0FF)
+val Blue = Color(0xFF6991BC)
+val TextBlue = Color(0xFF197488)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
