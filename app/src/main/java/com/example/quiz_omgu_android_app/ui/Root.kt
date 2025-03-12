caeeda6fa39f29c4c28f22f98cc044469c575b95
Egor Bugay <egbugaj@gmail.com>
2025-03-12 20:03:54 +0600

    feat: create test BottomNavigationBar

---
+++ app/src/main/java/com/example/quiz_omgu_android_app/ui/Root.kt
@@ -0,0 +1,68 @@
+package com.example.quiz_omgu_android_app.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.quiz_omgu_android_app.navigation.NavGraph
+import com.example.quiz_omgu_android_app.ui.navigation.BottomNavigationBar
+
+@Composable
+fun Root() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    var isNavBarVisible by remember { mutableStateOf(true) }
+    var scrollDirection by remember { mutableStateOf(0f) }
+    val scrollState = rememberLazyListState()
+
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                scrollDirection = available.y
+                return Offset.Zero
+            }
+        }
+    }
+
+    LaunchedEffect(scrollDirection, scrollState.firstVisibleItemScrollOffset) {
+        val newVisibility = scrollDirection > 0 || scrollState.firstVisibleItemScrollOffset == 0
+        if (isNavBarVisible != newVisibility) {
+            isNavBarVisible = newVisibility
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+            .nestedScroll(nestedScrollConnection)
+    ) {
+        NavGraph(
+            modifier = Modifier.fillMaxSize(),
+            navController = navController,
+            scrollState = scrollState,
+            onNavBarVisibilityChange = { isVisible -> isNavBarVisible = isVisible }
+        )
+
+        BottomNavigationBar(
+            navController = navController,
+            currentDestination = currentDestination,
+            isNavBarVisible = isNavBarVisible
+        )
+    }
+}
