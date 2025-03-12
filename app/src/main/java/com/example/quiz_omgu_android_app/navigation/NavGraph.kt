ea5eef57b3780b95ae3ef91a1f3e36168536ad33
Egor Bugay <egbugaj@gmail.com>
2025-03-12 20:03:32 +0600

    feat: implement NavGraph for navigation

---
+++ app/src/main/java/com/example/quiz_omgu_android_app/navigation/NavGraph.kt
@@ -0,0 +1,40 @@
+package com.example.quiz_omgu_android_app.navigation
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.quiz_omgu_android_app.ui.screens.create_room_screen.CreateRoomScreen
+import com.example.quiz_omgu_android_app.ui.screens.rooms_screen.RoomsScreen
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    scrollState: LazyListState,
+    onNavBarVisibilityChange: (Boolean) -> Unit,
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = RootScreens.Rooms.route
+    ) {
+        composable(route = RootScreens.Rooms.route) {
+            RoomsScreen(
+                onNavigateToCreateRoomScreen = {
+                    navController.navigate(RoomsScreens.CreateRoomScreen.route)
+                },
+                onNavBarVisibilityChange = onNavBarVisibilityChange
+            )
+        }
+        composable(route = RoomsScreens.CreateRoomScreen.route) {
+            CreateRoomScreen(
+                onNavBarVisibilityChange = onNavBarVisibilityChange,
+                onBackPressed = { navController.popBackStack() }
+            )
+        }
+    }
+}
